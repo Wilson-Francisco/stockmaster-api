a44f579dc58500c4238a42805c4cfaa2a8b36fa9
Wison Francisco <wilsonfranciscogamboa@yandex.ru>
2026-06-12 10:05:49 +0300

    feat(database): implementar funcao obter_conexao para o banco de dados

--- app/database.py
+++ app/database.py
@@ -6,3 +6,21 @@ from dotenv import load_dotenv
 
 # Carrega as configuracaoes secretas do arquivo .env
 load_dotenv()
+
+
+def obter_conexao():
+    """Abre e retorna uma conexao ativa com o banco de dados"""
+    try:
+        conexao = psycopg2.connect(
+            host = os.getenv("DB_HOST"),
+            port = os.getenv("DB_PORT"),
+            database = os.getenv("DB_NAME"),
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASSWORD"),
+            cursor_factory = RealDictCursor # Faz o banco retornar dados como dicionarios  
+        )
+
+        return conexao
+    except Exception as expt:
+        print(f"Erro critico: Nao foi possivel conectar ao banco: {expt}")
+        raise expt
